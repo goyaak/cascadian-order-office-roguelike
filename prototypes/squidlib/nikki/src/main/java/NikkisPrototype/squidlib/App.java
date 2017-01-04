380b378ddbd96d4b1138ec8ef5797d7678ea7d5f
kcnicole <kcnicole@192.168.0.100>
2017-01-03 19:36:21 -0800

    Switching from Gradle to Maven.
    Mostly, I'm just messing around with different configurations to get a
    better feel for how we can easily set these projects up.

---
+++ prototypes/squidlib/nikki/src/main/java/NikkisPrototype/squidlib/App.java
@@ -0,0 +1,13 @@
+package NikkisPrototype.squidlib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
