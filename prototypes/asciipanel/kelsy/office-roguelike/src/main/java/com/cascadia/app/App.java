e9a6c0ed14cbcf29d91f4b47dcd1aa10ca9cf7ca
Kelsy Parker <kparker@workwithopal.com>
2017-01-05 16:40:21 -0800

    Asciipanel implementation 'hello world' with a window that pops up

--- prototypes/asciipanel/kelsy/office-roguelike/src/main/java/com/cascadia/app/App.java
+++ prototypes/asciipanel/kelsy/office-roguelike/src/main/java/com/cascadia/app/App.java
@@ -1,13 +1,28 @@
 package com.cascadia.app;
 
+import javax.swing.JFrame;
+import asciiPanel.AsciiPanel;
 /**
- * Hello world!
+ * Basic "from scratch" roguelike based on a tutorial:
+ * http://trystans.blogspot.com/2016/01/roguelike-tutorial-00-table-of-contents.html
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App extends JFrame {
+  private static final long serialVersionUID = 1060623638149583738L;
+
+  private AsciiPanel terminal;
+
+  public App(){
+    super();
+    terminal = new AsciiPanel();
+    terminal.write("OFFICE ROGUELIKE GO GO", 1, 1);
+    add(terminal);
+    pack();
+  }
+
+  public static void main(String[] args) {
+    App app = new App();
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    app.setVisible(true);
+  }
 }
